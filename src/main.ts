10f90dca9d0b653761daa0a45d63bcc87061064b
Gary Sassano <10464497+garysassano@users.noreply.github.com>
2025-12-02 01:53:41 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { App } from "aws-cdk-lib";
+import { MyStack } from "./stacks/my-stack.js";
+
+const devEnv = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: "eu-west-1",
+};
+
+const app = new App();
+
+new MyStack(app, "cdk-aws-lmi-function", {
+  env: devEnv,
+});
+
+app.synth();
